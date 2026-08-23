9cce997f7d1fcddc28bed6233fd9f737e454f4fb
Nat822 <littlegirl2024s@gmail.com>
2026-08-23 20:15:20 +0200

    feat: expose repository adapters

---
+++ src/client_support/repositories/__init__.py
@@ -0,0 +1,11 @@
+from client_support.repositories.in_memory import (
+    InMemoryEventRepository,
+    InMemoryRunRepository,
+    InMemoryTicketRepository,
+)
+
+__all__ = [
+    "InMemoryEventRepository",
+    "InMemoryRunRepository",
+    "InMemoryTicketRepository",
+]
