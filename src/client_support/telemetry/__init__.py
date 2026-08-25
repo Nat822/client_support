b2b33939a27711210cd7a149982b18fff1302899
Nat822 <littlegirl2024s@gmail.com>
2026-08-25 06:27:14 +0200

    feat: expose structured agent telemetry

--- src/client_support/telemetry/__init__.py
+++ src/client_support/telemetry/__init__.py
@@ -1,3 +1,4 @@
+from client_support.telemetry.events import AgentEvent, InMemoryEventLog
 from client_support.telemetry.recorder import DomainEvent, EventRecorder
 
-__all__ = ["DomainEvent", "EventRecorder"]
+__all__ = ["AgentEvent", "InMemoryEventLog", "DomainEvent", "EventRecorder"]
